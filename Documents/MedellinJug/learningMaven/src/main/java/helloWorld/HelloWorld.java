54e79303ea63f3c9c46f7c464904bd720d094ebf
PatriciaUribeZuleta <PatriciaUribeZuleta@172-0-9-36.lightspeed.wlfrct.sbcglobal.net>
2018-03-27 12:57:05 -0500

    Hello World! in Maven

---
+++ Documents/MedellinJug/learningMaven/src/main/java/helloWorld/HelloWorld.java
@@ -0,0 +1,22 @@
+package helloWorld;
+
+/*
+*  medellinjug.org
+*
+*  27/03/2018
+*
+*  Patricia UZ
+*
+* */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+    System.out.println("Hello world");
+
+    //examples
+
+    }
+
+}
