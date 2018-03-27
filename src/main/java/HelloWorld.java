34453c3095ee3cb08918f9009d2690b32a6c35b0
PatriciaUribeZuleta <paurzu@hotmail.com>
2018-03-27 12:03:14 -0500

    First commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,16 @@
+/*
+* medellinjug.org
+*
+*
+* PatriciaUZ
+* */
+
 public class HelloWorld {
 
     public  static  void main(String[] args){
 
         System.out.println("Hello world");
-
+        //examples
     }
 
 }
