0a0551b0da120a5bfee08ce912f64fd6e7760f47
PatriciaUribeZuleta <PatriciaUribeZuleta@172-0-9-36.lightspeed.wlfrct.sbcglobal.net>
2018-03-28 08:26:34 -0500

    project with Maven structure

---
+++ Documents/MedellinJug/HelloWorldMaven/src/main/java/org/medellinjug/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.medellinjug;
+
+public class HelloWorld {
+
+    public  static void main(String... args){
+
+        System.out.println("Hello world");
+
+        //Other examples
+    }
+}
---
+++ Documents/MedellinJug/HelloWorldMaven/src/test/java/org/medellinjug/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package org.medellinjug;
+
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void myTest(){
+        HelloWorld.main();
+
+
+    }
+}
